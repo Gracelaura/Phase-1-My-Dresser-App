693fefa5a49b8b7ac68f63f8112ea4c3a39f2d30
Gracelaura <gracenjuguna05@gmail.com>
2022-09-02 02:10:37 +0300

    added a json server

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,19 +1,27 @@
 function myFunction() {
-    let dropdown = 
-
-    document.getElementById("myDropdown").classList.toggle("show");
+    let dropdown = document.getElementById("myDropdown")
+      if(dropdown.style.display=="block"){
+        dropdown.style.display="none"
+      }else{
+        dropdown.style.display="block"
+      }
 }
-function myFunction2() {
-    document.getElementById("myForm").submit();
-  }
-
+ function myFunction2() {
+     document.getElementById("myForm").addEventListener("submit",commentsFunction)
+}
+let commentsFunction = (e) => {
+  e.preventDefault()
+  let commentsDisplay = getElementById("comments-display")
+  let comment = `<p>${e.target.comment.value}</p>`
+  commentsDisplay.innerHTML += comment
+}
 const list = document.getElementById('list')
   list.addEventListener('click',function(){
     document.getElementById('categoriesjs').style.display="block";
     document.getElementById('commentsjs').style.display="none";
 })
 const form = document.getElementById('myForm')
-  interpreter.addEventListener('click',function(){
+  myForm.addEventListener('click',function(){
     document.getElementById('commentsjs').style.display="block";
     document.getElementById('categoriesjs').style.display="none";
 })
@@ -30,3 +38,51 @@ window.onclick = function(event) {
     }
   }
 
+fetch("http://localhost:3000/outfits/1")
+.then(response => response.json())
+.then(data =>{
+  updatePage(data);
+})
+
+function updatePage(outfitsObj){
+let swankyImg = document.getElementById("swanky-outfit");
+swankyImg.src = outfitsObj.image1;
+document.getElementById("swanky-outfit2").src = outfitsObj.image2
+}
+
+fetch("http://localhost:3000/outfits/2")
+.then(response => response.json())
+.then(data =>{
+  console.log(data);
+  updateChic(data);
+})
+
+function updateChic(outfitsObj){
+let chicImg = document.getElementById("chic-outfit");
+chicImg.src = outfitsObj.image1;
+document.getElementById("chic-outfit2").src = outfitsObj.image2
+}
+fetch("http://localhost:3000/outfits/3")
+.then(response => response.json())
+.then(data =>{
+  console.log(data);
+  updateCasual(data);
+})
+
+function updateCasual(outfitsObj){
+let casualImg = document.getElementById("casual-outfit");
+casualImg.src = outfitsObj.image1;
+document.getElementById("casual-outfit2").src = outfitsObj.image2
+}
+fetch("http://localhost:3000/outfits/4")
+.then(response => response.json())
+.then(data =>{
+  console.log(data);
+  updateRockstar(data);
+})
+
+function updateRockstar(outfitsObj){
+let rockstarImg = document.getElementById("rockstar-outfit");
+rockstarImg.src = outfitsObj.image1;
+document.getElementById("rockstar-outfit2").src = outfitsObj.image2
+}
