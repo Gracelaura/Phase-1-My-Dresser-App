b479af1ae5e81516e0e0ce19bb0847d58b542021
Gracelaura <gracenjuguna05@gmail.com>
2022-09-04 18:14:20 +0300

    Added changes to all files and folders

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,7 @@
 window.addEventListener("DOMContentLoaded", () => {
-  document.querySelector("#myForm").addEventListener("submit",(e) =>{
+  document.querySelector("#myForm").addEventListener("submit", (e) => {
     e.preventDefault();
-    //alert("hello")
+    // alert("hello")
     addComment();
 
     function addComment() {
@@ -9,19 +9,19 @@ window.addEventListener("DOMContentLoaded", () => {
       console.log(inputComment);
       let li = document.createElement("li");
       li.textContent = inputComment;
-      let displayedComment = document.querySelector("#comments-displayed");
+      let displayedComment = document.querySelector("#comments-display");
 
-      displayedComment.appendChild("li");
+      displayedComment.appendChild(li);
       li.style.listStyle = "none";
       myForm.reset();
     }
   });
 
   document.querySelector(".dropbtn").addEventListener("click", (e) => {
-    let categoryShow = document.querySelector("#categoryjs");
+    let categoryShow = document.querySelector("#catergoriesjs");
     if (categoryShow.style.display == "block") {
       categoryShow.style.display = "none";
-    }else{
+    } else {
       categoryShow.style.display = "block";
 
       let showBanner = document.getElementById("showBanner");
@@ -30,33 +30,37 @@ window.addEventListener("DOMContentLoaded", () => {
       } else {
         showBanner.style.display = "none";
       }
-      }
-    });
-
-        //FETCH DATA FROM LOCALHOST (db.json)
-    fetch("http://localhost:3000/outfits")
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-          //SWANKY CATEGORY
-        document.getElementById("swanky-title").textContent = data[0].category;
-        document.getElementById("swanky-outfit").src = data[0].image1;
-        document.getElementById("swanky-outfit2").src = data[0].image2;
-        document.getElementById("swanky-description").textContent = data[0].description;
-        //CHIC CATEGORY
-        document.getElementById("chic-title").textContent = data[1].category;
-        document.getElementById("casual-outfit").src = data[2].image1;
-        document.getElementById("casual-outfit2").src = data[2].image2;
-        document.getElementById("casual-description").textContent = data[2].description;
-        //CASUAL CATEGORY
-        document.getElementById("casual-title").textContent = data[2].category;
-        document.getElementById("casual-outfit").src = data[2].image1;
-        document.getElementById("casual-outfit2").src = data[2].image2;
-        document.getElementById("casual-description").textContent = data[2].description;
-        //ROCKSTAR CATEGORY
-        document.getElementById("rockstar-title").textContent = data[3].category;
-        document.getElementById("rockstar-outfit").src = data[3].image1;
-        document.getElementById("rockstar-outfit2").src = data[3].image2;
-        document.getElementById("rockstar-description").textContent = data[3].description;
-      });
+    }
   });
+
+  fetch("http://localhost:3000/outfits")
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+
+      document.getElementById("swanky-title").textContent = data[0].category;
+      document.getElementById("swanky-outfit").src = data[0].image1;
+      document.getElementById("swanky-outfit2").src = data[0].image2;
+      document.getElementById("swanky-description").textContent =
+        data[0].description;
+
+      document.getElementById("chic-title").textContent = data[1].category;
+      document.getElementById("chic-outfit").src = data[1].image1;
+      document.getElementById("chic-outfit2").src = data[1].image2;
+      document.getElementById("chic-description").textContent =
+        data[1].description;
+
+      document.getElementById("casual-title").textContent = data[2].category;
+      document.getElementById("casual-outfit").src = data[2].image1;
+      document.getElementById("casual-outfit2").src = data[2].image2;
+      document.getElementById("casual-description").textContent =
+        data[2].description;
+
+      document.getElementById("rockstar-title").textContent = data[3].category;
+      document.getElementById("rockstar-outfit").src = data[3].image1;
+      document.getElementById("rockstar-outfit2").src = data[3].image2;
+      document.getElementById("rockstar-description").textContent =
+        data[3].description;
+    });
+});
+
